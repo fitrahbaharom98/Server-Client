e23552d2783685920b9cb48be2cc0250b71ebf83
fitrahbaharom98 <48412872+fitrahbaharom98@users.noreply.github.com>
2019-03-20 17:21:24 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,45 +1,46 @@
-import java.io.*;
 
-import java.net.*;
-
-class client1
-{
-  public static void main(String[] args) throws Exception
-  {
-     Socket sock = new Socket("192.168.40.132", 9706);
-
-                               // reading from keyboard (keyRead object)
-
-     BufferedReader keyRead = new BufferedReader(new 
-      InputStreamReader(System.in));
-
-                              // sending to client (pwrite object)
-
-     OutputStream ostream = sock.getOutputStream(); 
-      PrintWriter pwrite = new PrintWriter(ostream, true);
-
-                              // receiving from server ( receiveRead  object)
-
-     InputStream istream = sock.getInputStream();
-      BufferedReader receiveRead = new BufferedReader(new 
-      InputStreamReader(istream));
-
-     System.out.println("Waiting for client.... Please wait! :) )");
-
-     String receiveMessage, sendMessage;               
-
-     while(true)
-
-     {
-
-        sendMessage = keyRead.readLine();  // keyboard reading
-         pwrite.println(sendMessage);       // sending to server
-         pwrite.flush();
-	
-	if((receiveMessage = receiveRead.readLine()) != null)
-	{
-		System.out.println("Server: " + receiveMessage);
-	}
-}
-}
-}
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#include <unistd.h> 
+#include <arpa/inet.h>
+#define PORT 5678
+   
+int main(int argc, char const *argv[]) 
+{ 
+    struct sockaddr_in address; 
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "Assalamualaikum from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    memset(&serv_addr, '0', sizeof(serv_addr)); 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "192.168.211.132", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nAddress not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnected Failed\n"); 
+        return -1; 
+    } 
+    send(sock , hello , strlen(hello) , 0 ); 
+    printf("Yeayy!! You are successfully connected!\n"); 
+    valread = read( sock , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    return 0; 
+} 

bb24bc4ac57d6b72a3838467144f167bc3ed9720
fitrahbaharom98 <48412872+fitrahbaharom98@users.noreply.github.com>
2019-03-20 17:58:48 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,9 +6,9 @@ class Server
 public static void main(String[] args)throws Exception
 {
 ServerSocket serversocket = new ServerSocket(6789);
-System.out.println("Welcome! Server is ready!");
+System.out.println("Welcome! Server is ready to start now!");
 Socket sock = serversocket.accept();
-System.out.println("Waalaikummussalam");
+System.out.println("whats up!");
 
 BufferedReader keyRead = new BufferedReader(new 
 InputStreamReader(System.in));
